64666e22420806e616e719ad75f284f626ca01cd
Pedro Victor <37453874+stormsamurai1@users.noreply.github.com>
2019-10-24 16:36:34 -0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,7 @@ const routes = require('./routes')
 
 const app = express();
 
-mongoose.connect("mongodb+srv://omnistack:omnistack@cluster0-tjl7o.mongodb.net/test?retryWrites=true&w=majority", { 
+mongoose.connect("mongodb+srv://omnistack:{/*password*/}@cluster0-tjl7o.mongodb.net/test?retryWrites=true&w=majority", { 
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
